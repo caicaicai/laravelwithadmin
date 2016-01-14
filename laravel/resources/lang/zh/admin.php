ef41630889f00381cf29f5548ba6f8ff9dfdaca2
蔡忠振 <94763163@qq.com>
2016-01-14 18:02:01 +0800

    lan

---
+++ laravel/resources/lang/zh/admin.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+	'menus' => [
+		'dashboard' => '仪表盘',
+		'articles' => [
+			'title' => '文档',
+			'all' => '所有文档',
+			'create' => '增加文档'
+		],
+		'pages' => [
+			'title' => '页面',
+			'all' => '所有页面',
+			'create' => '增加页面'
+		],
+		'users' => [
+			'title' => '用户',
+			'all' => '所有用户',
+			'create' => '增加用户'
+		],
+		'roles' => '角色',
+		'permissions' => '权限',
+		'categories' => '分类'
+	]
+];
